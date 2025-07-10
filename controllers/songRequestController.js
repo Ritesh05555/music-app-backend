3045ca886227c14909ed7fb22bf9162728f1e121
RITESH SAHANI <149089288+Ritesh05555@users.noreply.github.com>
2025-07-10 17:42:32 +0530

    Update songRequestController.js

--- controllers/songRequestController.js
+++ controllers/songRequestController.js
@@ -1,39 +1,3 @@
-// const SongAddRequest = require('../models/songAddRequest');
-
-// const createSongRequest = async (req, res) => {
-//   try {
-//     const { title, movie } = req.body;
-
-//     if (!title || !movie) {
-//       return res.status(400).json({ message: 'Please provide both song name and movie name.' });
-//     }
-
-//     const newRequest = new SongAddRequest({
-//       title,
-//       movie,
-//       requestedBy: req.user.id
-//     });
-
-//     await newRequest.save();
-
-//     res.status(201).json({ message: 'Request submitted successfully', request: newRequest });
-//   } catch (error) {
-//     console.error('Create song request error:', error.message);
-//     res.status(500).json({ message: 'Server error', error: error.message });
-//   }
-// };
-
-// const getAllSongRequests = async (req, res) => {
-//   try {
-//     const requests = await SongAddRequest.find().populate('requestedBy', 'fullName email');
-//     res.json(requests);
-//   } catch (error) {
-//     console.error('Get song requests error:', error.message);
-//     res.status(500).json({ message: 'Server error', error: error.message });
-//   }
-// };
-
-// module.exports = { createSongRequest, getAllSongRequests };
 const SongAddRequest = require('../models/songAddRequest');
 
 const createSongRequests = async (req, res) => {
